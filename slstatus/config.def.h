b4f792df13816a443a0163f785f3b4f7e1e3deef
Ted <tedjreimer@gmail.com>
2024-11-05 21:53:53 -0600

    Fixed the volume display

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -66,7 +66,7 @@ static const char unknown_str[] = "n/a";
 static const struct arg args[] = {
 	/* function format               argument */
     // TODO: Have this consider if the volume is muted.
-    { run_command,  " Volume: %s |",        "~/.tedsconfigs/extras/volume.sh'"},
+    { run_command,  " Volume: %s |",        "/home/ted/.tedsconfigs/extras/volume.sh"},
     { wifi_essid,   " %s ",           "wlp0s20f3"},
     { wifi_perc,    "(%s%%) | ",     "wlp0s20f3"},
     { battery_perc, "%s%% Battery | ",       "BAT0"},
